6696d9d1b7118a90ba5361fd295cb9a5834238ac
MatheusBenedikt <108495908+MatheusBenedikt@users.noreply.github.com>
2026-04-26 21:16:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Eu te amo meu amor, você é a minha laranjinha! 😎</h1>
+function Home() {
+  return <h1>Eu te amo meu amor, você é a minha laranjinha! 😎</h1>;
 }
 
 export default Home;
